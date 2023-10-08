ff8ab441645968e616eede8c1b52d20225532770
shikhayadav09 <144312725+shikhayadav09@users.noreply.github.com>
2023-10-08 13:28:01 +0530

    Update Prime factorization.java

--- Prime factorization.java
+++ Prime factorization.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class Prime_factorization {
     public static void main(String[] args) {
-
-        Scanner sc=new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Input a number: ");
